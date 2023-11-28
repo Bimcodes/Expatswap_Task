80d5df2403bc80fca80545f03b340c45415c53c0
BB <akinuliolaolami@gmail.com>
2023-11-28 01:16:07 +0100

    I encountered an error while compiling so i updated it

--- lib/information_sheet.dart
+++ lib/information_sheet.dart
@@ -1,9 +1,9 @@
+import 'package:expatswap_task/widgets/button.dart';
+import 'package:expatswap_task/widgets/text.dart';
+import 'package:expatswap_task/widgets/text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:expatswap_task/button.dart';
 import 'package:expatswap_task/constants/colors.dart';
 // import 'package:inom/dashborad.dart';
-import 'package:expatswap_task/text.dart';
-import 'package:expatswap_task/text_field.dart';
 import 'package:expatswap_task/user_data.dart';
 import 'package:provider/provider.dart';
 
@@ -56,57 +56,93 @@ class _InformationState extends State<Information> {
                 )
               : Form(
                   key: _formKey,
-                  child: ListView(
+                  child: Column(
                     // crossAxisAlignment: CrossAxisAlignment.start,
                     // mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       text('Name'),
-                      textField(nameController, TextInputType.name,
-                          'Enter your name', false, (value) {
-                        userData.userName = value;
-                      }, hintText: 'Abimbola'),
-                      const Spacer(),
+                      TextFormField(
+                        controller: nameController,
+                        onChanged: (value) {
+                          userData.userName = value;
+                        },
+                      ),
+                      // textField(nameController, TextInputType.name,
+                      //     'Enter your name', false, (value) {
+                      //   userData.userName = value;
+                      // }, hintText: 'Abimbola'),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       text('Email Address'),
-                      textField(emailController, TextInputType.emailAddress,
-                          'Enter valid email address', true, (value) {
-                        userData.email = value;
-                      }, hintText: 'example@gmail.com', isEmail: true),
-                      const Spacer(),
-                      text('Phone Number'),
-                      textField(phoneNumberController, TextInputType.number,
-                          'Enter your phone number', false, (value) {
-                        userData.phoneNumber = value;
-                      }, hintText: '+2349017250087'),
-                      const Spacer(),
-                      text('Date of Birth'),
-                      textField(dateOfBirthController, TextInputType.datetime,
-                          'Enter your date of birth', false, (value) {
-                        userData.dateOfBirth = value;
-                      }, hintText: '24/07/1642'),
-                      const Spacer(),
-                      text('Address'),
-                      textField(addressController, TextInputType.streetAddress,
-                          'Enter your address', false, (value) {
-                        userData.address = value;
-                      }, hintText: '24, Ogablumo street, Lagos, Nigeria'),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(left: 50, right: 50, top: 18),
-                        child: button(() {
-                          _formKey.currentState!.validate() ? _submit() : null;
-                          userData.updateUserData(
-                              userData.userName,
-                              userData.email,
-                              userData.phoneNumber,
-                              userData.dateOfBirth,
-                              userData.address);
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //       builder: (context) => const DashBoard()),
-                          // );
-                        }, 'Submit', 50, 327, white, blue, link),
+                      TextFormField(
+                        controller: emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          userData.email = value;
+                        },
                       )
+                      // textField(emailController, TextInputType.emailAddress,
+                      //     'Enter valid email address', true, (newValue) {
+                      //   userData.email = newValue;
+                      // }, hintText: 'example@gmail.com', isEmail: true),
+                      ,
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      text('Phone Number'),
+                      // textField(phoneNumberController, TextInputType.number,
+                      //     'Enter your phone number', false, (value) {
+                      //   userData.phoneNumber = value;
+                      // }, hintText: '+2349017250087'),
+                      TextFormField(
+                        controller: phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        onChanged: (value) {
+                          userData.phoneNumber = value;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      text('Date of Birth'),
+                      // textField(dateOfBirthController, TextInputType.datetime,
+                      //     'Enter your date of birth', false, (value) {
+                      //   userData.dateOfBirth = value;
+                      // }, hintText: '24/07/1642'),
+                      TextFormField(
+                        controller: dateOfBirthController,
+                        keyboardType: TextInputType.datetime,
+                        onChanged: (value) {
+                          userData.dateOfBirth = value;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      text('Address'),
+                      //
+                      TextFormField(
+                        controller: addressController,
+                        keyboardType: TextInputType.streetAddress,
+                        onChanged: (value) {
+                          userData.address = value;
+                        },
+                      ),
+                      button(() {
+                        _formKey.currentState!.validate() ? _submit() : null;
+                        userData.updateUserData(
+                            userData.userName,
+                            userData.email,
+                            userData.phoneNumber,
+                            userData.dateOfBirth,
+                            userData.address);
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //       builder: (context) => const DashBoard()),
+                        // );
+                      }, 'Submit', 50, 327, white, blue, link)
                     ],
                   ),
                 ),
